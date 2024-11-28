3323bc1f9bf8acac4261e63540e1320b4798e014
Promise-Digitals <promiseedeh025@gmail.com>
2024-11-28 09:57:26 +0100

    Navbar Section Completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Doctors from './Pages/Doctors'
+import Login from './Pages/Login'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import MyProfile from './Pages/MyProfile'
+import MyApointments from './Pages/MyApointments'
+import Appointments from './Pages/Appointments'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'> 
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/appointments/:docId' element={<Appointments />} />
+        <Route path='/my-appointments' element={<MyApointments />} />
+      </Routes>
+
     </div>
   )
 }
---
+++ Frontend/src/Pages/MyApointments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyApointments = () => {
+    return (
+        <div> 
+
+        </div>
+    )
+}
+
+export default MyApointments
