4bf6136f62e6912ebd696359fc93be6437d6e8a5
Promise-Digitals <promiseedeh025@gmail.com>
2024-11-30 07:51:15 +0100

    Appointment page completed

--- Frontend/src/Pages/Appointments.jsx
+++ Frontend/src/Pages/Appointments.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 const Appointments = () => {
 
@@ -129,7 +130,12 @@ const Appointments = () => {
                         </p>
                     ))}
                 </div>
+
+                <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full mt-8'>Book an appointment</button>
             </div>
+
+            {/* ---------- Listing Related Doctors ------------ */}
+            <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
         </div>
     )
 }
