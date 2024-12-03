2522aee3cf8d81428b48b771c4f4e0e4b1f8435a
Promise-Digitals <promiseedeh025@gmail.com>
2024-12-03 07:01:23 +0100

    mongoDB connection

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()
+
+
+// Middlewares
+app.use(express.json());
+app.use(cors())
+
+// API endpoints
+app.get('/', (req, res) => {
+    res.send("API WORKING")
+})
+
+
+app.listen(port, () => {
+    console.log("Server Started ", port)
+})
