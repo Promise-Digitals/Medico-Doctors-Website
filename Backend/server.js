f4d4547bb35b74c27274dd12978e878a3624ef2c
Promise-Digitals <promiseedeh025@gmail.com>
2024-12-04 06:59:20 +0100

    models and multer created

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 
 // app config
@@ -20,6 +21,9 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send("API WORKING")
 })
+
+app.use('/api/admin', adminRouter)
+
 
 
 app.listen(port, () => {
