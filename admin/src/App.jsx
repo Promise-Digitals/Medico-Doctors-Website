2677b97db1c126c116a1fe99b7a9bbf3a5b8562d
Promise-Digitals <promiseedeh025@gmail.com>
2024-12-07 07:05:45 +0100

    Sidebar UI

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,14 +4,32 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useContext } from 'react';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor'
+import DoctorsList from './pages/Admin/DoctorsList'
 
 const App = () => {
 
   const {aToken} = useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#f8f9fd]'>
       <ToastContainer />
+      <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ): (
     <>
