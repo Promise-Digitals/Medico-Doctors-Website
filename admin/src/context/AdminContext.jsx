22c2327fc0c589c68ecbaba838a524ec859305be
Promise-Digitals <promiseedeh025@gmail.com>
2024-12-08 16:26:37 +0100

    change availability functionality

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -18,7 +18,6 @@ const AdminContextProvider = (props) => {
             const {data} = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers: {aToken}})
             if(data.success){
                 setDoctors(data.doctors)
-                console.log(data.doctors)
 
             }else{
                 toast.error(data.message)
@@ -28,11 +27,32 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const changeAvailablity = async (docId) => {
+        try {
+            
+            const {data} = await axios.post(backendUrl + '/api/admin/change-availability', {docId}, {headers: {aToken}})
+            console.log('error')
+            if (data.success) {
+                toast.success(data.message)
+                getAllDoctors()
+            }else{
+                toast.error(data.message)
+            }
+
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+
     const value = {
         aToken, setAToken,
         backendUrl,
         doctors,
-        getAllDoctors
+        getAllDoctors,
+        changeAvailablity
     }
 
 
