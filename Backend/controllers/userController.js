e2df06d91ffcefb652c3886756da64ab3d2860ef
Promise-Digitals <promiseedeh025@gmail.com>
2024-12-19 21:24:38 +0100

    my appointments API

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -239,5 +239,26 @@ const bookAppointment = async (req, res) => {
     }
 }
 
+// API to get user appointments for frontend my-appointments page
+const listAppointment = async (req, res) => {
+    try {
 
-export {registerUser, loginUser, getProfile, updateProfile, bookAppointment}
+        const {userId} = req.body
+        const appointments = await appointmentModel.find({userId})
+        
+        res.json({
+            success: true,
+            appointments
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment}
