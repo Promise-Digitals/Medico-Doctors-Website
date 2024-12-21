fedcb41b7849c2bd22beb2efc38a7489c1c9839b
Promise-Digitals <promiseedeh025@gmail.com>
2024-12-21 15:56:36 +0100

    All appointments admin API

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
+import appointmentModel from '../models/appointmentModel.js'
 
 
 // API for adding doctor
@@ -117,6 +118,26 @@ const allDoctors = async (req, res) => {
             doctors
         })
 
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+// API to get all appointments list
+const appointmentsAdmin = async (req, res) => {
+    try {
+
+        const appointments = await appointmentModel.find({})
+        res.json({
+            success: true,
+            appointments
+        })
+        
     } catch (error) {
         console.log(error)
         res.json({
