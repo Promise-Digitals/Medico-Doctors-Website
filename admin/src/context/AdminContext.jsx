19c3ff4cd959ab69d89517b524b79f9f1d028a0c
Promise-Digitals <promiseedeh025@gmail.com>
2024-12-21 16:50:24 +0100

    admin cancel appointment API

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : "");
 
     const [doctors, setDoctors] = useState([])
+    const [appointments, setAppointments] = useState([])
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 
@@ -46,13 +47,34 @@ const AdminContextProvider = (props) => {
     }
 
 
+    const getAllAppointments = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + "/api/admin/appointments", {headers: {aToken}})
+
+            if (data.success) {
+                setAppointments(data.appointments)
+                console.log(data.appointments)
+            }else{
+                toast.error(data.message)
+            }
+
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
 
     const value = {
         aToken, setAToken,
         backendUrl,
         doctors,
         getAllDoctors,
-        changeAvailablity
+        changeAvailablity,
+        appointments, setAppointments,
+        getAllAppointments
     }
 
 
