329106e8e44a314cc4b49713f6f54cbde9968700
Promise-Digitals <promiseedeh025@gmail.com>
2025-01-04 22:48:34 +0100

    doctor admin section

--- Backend/routes/doctorRoute.js
+++ Backend/routes/doctorRoute.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { doctorList } from '../controllers/doctorController.js'
+import { doctorList, loginDoctor } from '../controllers/doctorController.js'
 
 
 const doctorRouter = express.Router()
 
 
 doctorRouter.get('/list', doctorList)
+doctorRouter.post('/login', loginDoctor)
+
 
 
 
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { AdminContext } from '../context/AdminContext';
 import axios from 'axios'
 import { toast } from 'react-toastify';
+import { DoctorContext } from '../context/DoctorContext';
 
 const Login = () => {
 
@@ -11,6 +12,7 @@ const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const {setAToken, backendUrl} = useContext(AdminContext);
+    const {setDToken} = useContext(DoctorContext)
 
 
     const onSubmitHandler = async (e) => {
@@ -27,7 +29,18 @@ const Login = () => {
                 }else{
                     toast.error(data.message)
                 }
+
             }else{
+
+                const {data} = await axios.post(backendUrl + '/api/doctor/login', {email, password})
+                if (data.success) {
+                    localStorage.setItem('dToken', data.token)
+                    setDToken(data.token)
+                    console.log(data.token)
+                    
+                }else{
+                    toast.error(data.message)
+                }
 
             }
         } catch (error) {
