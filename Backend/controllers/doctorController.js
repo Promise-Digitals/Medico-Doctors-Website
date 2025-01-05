6e7eef9c4409541b2989def56d9175c87060f652
Promise-Digitals <promiseedeh025@gmail.com>
2025-01-05 14:04:10 +0100

    complete and cancel appointment functionalities

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -1,6 +1,7 @@
 import doctorModel from "../models/doctorModel.js"
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModel.js"
 
 
 
@@ -90,5 +91,93 @@ const loginDoctor = async (req, res) => {
 
 
 
+// API to get doctor appointments for doctor panel
+const appointmentsDoctor = async (req, res) => {
+    
+    try {
 
-export {changeAvailability, doctorList, loginDoctor}
+        const { docId } = req.body
+        const appointments = await appointmentModel.find({docId})
+
+        res.json({
+            success: true,
+            appointments
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+// APi to mark appointment completed for doctor panel
+const appointmentComplete = async (req, res) => {
+    try {
+
+        const {docId, appointmentId} = req.body
+
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        if (appointmentData && appointmentData.docId === docId) {
+            
+            await appointmentModel.findByIdAndUpdate(appointmentId, {isCompleted: true})
+
+            return res.json({
+                success: true,
+                message: 'Appointment Completed'
+            })
+        }else{
+            return res.json({
+                success: false,
+                message: 'Mark Failed'
+            })
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+// APi to cancel appointment for doctor panel
+const appointmentCancel = async (req, res) => {
+    try {
+
+        const {docId, appointmentId} = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        if (appointmentData && appointmentData.docId === docId) {
+            
+            await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled: true})
+
+            return res.json({
+                success: true,
+                message: 'Appointment Cancelled'
+            })
+        }else{
+            return res.json({
+                success: false,
+                message: 'Cancellation failed'
+            })
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export {changeAvailability, doctorList, loginDoctor, appointmentsDoctor, appointmentCancel, appointmentComplete}
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,7 +16,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'> 
-      <ToastContainer />
+      <ToastContainer theme='colored' hideProgressBar={true} closeOnClick={true} />
       <Navbar />
 
       <Routes>
